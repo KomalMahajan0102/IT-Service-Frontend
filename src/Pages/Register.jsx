f485da1f22194d71ac34e94fe49efc03c0679bcc
KomalMahajan0102 <127200045+KomalMahajan0102@users.noreply.github.com>
2024-06-25 12:31:47 +0530

    Update Register.jsx

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -18,7 +18,7 @@ const Register = () => {
 
     console.log(user);
     try{
-      const response=await fetch("http://localhost:5000/api/auth/register",{
+      const response=await fetch("https://it-service-app-api.onrender.com/api/auth/register",{
         method:"POST",
         headers:{
           'Content-Type':"application/json"
